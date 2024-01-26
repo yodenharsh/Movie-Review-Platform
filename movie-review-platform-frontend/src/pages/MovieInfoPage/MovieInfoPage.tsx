d41f3ee838f987b611cb38c3d057d0be784a9322
Harsh Morayya <harshmorayya@ymail.com>
2024-01-26 15:06:42 +0530

    Modified movie page layout

--- movie-review-platform-frontend/src/pages/MovieInfoPage/MovieInfoPage.tsx
+++ movie-review-platform-frontend/src/pages/MovieInfoPage/MovieInfoPage.tsx
@@ -1,12 +1,21 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import ContentContainerCard from "../../components/ContentContainerCard";
+import {
+  faThumbsUp,
+  faStar,
+  faPercent,
+  faThumbsDown,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbsUp, faStar } from "@fortawesome/free-solid-svg-icons";
+
+import RatingCard from "./RatingCard";
+import React from "react";
 
 interface MovieDetails {
   title: string;
   summary: string;
   releaseDate: string;
+  directorList: string[];
   actorList: string[];
   genre: string;
   likeCount: number;
@@ -23,56 +32,138 @@ const MovieDetailsPage: FC = () => {
     "Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.";
   const releaseDate = "2023-10-03";
   const actorList: string[] = ["Nathan Parker", "Maria Jose", "Geeman Bro"];
-  const genre: string[] = ["superhero", "indian"];
+  const genre: string[] = ["Superhero", "Indian"];
   const score: number = 4.9;
   const likeCount = 99991;
   const dislikeCount = 42332;
+  const directorList = ["Micheal Jordan"];
 
   const totalVotes = likeCount + dislikeCount;
   const likePercentage =
     totalVotes > 0 ? Math.round((likeCount / totalVotes) * 100) : 0;
 
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+  const [rating, setRating] = useState(0);
+
+  const handleLike = () => {
+    setLikes(likes + 1);
+  };
+
+  const handleDislike = () => {
+    setDislikes(dislikes + 1);
+  };
+
+  const handleRating = (value: React.SetStateAction<number>) => {
+    setRating(value);
+  };
+
   return (
     <div className="mx-auto flex justify-center my-12">
       <ContentContainerCard>
-        <div className="flex text-white p-12">
+        <div className="flex text-white py-12 pl-12 pr-4">
           {/* Left Pane: Movie Poster */}
           <div className="w-1/4">
             <img src={imgUrl} alt={title} className="w-full h-auto" />
+            <div className="flex justify-between mt-4">
+              <button
+                onClick={handleLike}
+                className="bg-green-500 text-white p-2 rounded-md"
+              >
+                <FontAwesomeIcon icon={faThumbsUp} /> Like
+              </button>
+              <button
+                onClick={handleDislike}
+                className="bg-red-500 text-white p-2 rounded-md"
+              >
+                <FontAwesomeIcon icon={faThumbsDown} /> Dislike
+              </button>
+            </div>
+
+            {/* Rating System */}
+            <div className="mt-4">
+              <strong>Your Rating:</strong>{" "}
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+                <button
+                  key={value}
+                  type="button"
+                  title="score"
+                  onClick={() => handleRating(value)}
+                  className={`text-xl mx-1 ${
+                    rating >= value ? "text-yellow-500" : "text-gray-300"
+                  }`}
+                >
+                  <FontAwesomeIcon icon={faStar} />
+                </button>
+              ))}
+            </div>
           </div>
 
           {/* Right Pane: Movie Details */}
-          <div className="w-3/4 grid grid-cols-9 p-4">
-            <div className="col-span-7">
-              <h1 className="text-3xl font-bold">{title}</h1>
-              <p className="text-gray-600">{summary}</p>
+          <div className="w-3/4 grid grid-cols-9 py-4 pl-4 px-0 bg-black ml-4">
+            <div className="col-span-7 p-6 ">
+              <div className="bg-gray-800 rounded-md px-4 pt-4">
+                <h1 className="text-3xl font-bold  mb-4 border-b-4 border-black">
+                  {title}
+                </h1>
 
-              <div className="mt-4">
-                <strong>Release Date:</strong> {releaseDate}
+                {/* Movie Summary Card */}
+                <div className="pb-4 rounded-md mb-4">
+                  <p className="text-gray-300">{summary}</p>
+                </div>
               </div>
 
-              <div className="mt-2">
-                <strong>Actor List:</strong> {actorList.join(", ")}
+              <div className="bg-gray-800 p-4 rounded-md mb-4">
+                <div className="mb-4">
+                  <strong>Release Date:</strong> {releaseDate}
+                </div>
+
+                <hr className="border-t border-white mb-4" />
+
+                <div className="mb-4">
+                  <strong>Actors:</strong> {actorList.join(" ● ")}
+                </div>
+
+                <hr className="border-t border-white mb-4" />
+
+                <div>
+                  <strong>Directors:</strong> {directorList.join(" ● ")}
+                </div>
               </div>
 
-              <div className="mt-2">
-                <strong>Genre:</strong> {genre.join(", ")}
+              {/* Genre Card */}
+              <div className="bg-gray-800 rounded-md">
+                <div className="flex items-center font-semibold text-center p-2">
+                  {genre.map((item, index) => (
+                    <React.Fragment key={index}>
+                      {index > 0 && (
+                        <div className="border-r mx-2 h-5 bg-white opacity-50"></div>
+                      )}
+                      <span className=" flex-grow">{item}</span>
+                    </React.Fragment>
+                  ))}
+                </div>
               </div>
             </div>
-            <div className="col-span-2">
-              <div className="mt-2 flex items-center">
-                <strong>Approval:</strong>{" "}
-                <span className="ml-2 text-5xl text-green-500">
-                  {likePercentage}% <FontAwesomeIcon icon={faThumbsUp} />
-                </span>
-              </div>
 
-              <div className="mt-2 flex items-center">
-                <strong>Score:</strong>
-                <br /> {score}/10{" "}
-                <span className="ml-2 text-yellow-500">
-                  <FontAwesomeIcon icon={faStar} />
-                </span>
+            <div className="col-span-2">
+              <div className="">
+                <div className="p-4">
+                  <RatingCard
+                    title="Approval"
+                    value={likePercentage}
+                    icon={faPercent}
+                    color="text-green-500"
+                  />
+                </div>
+                <div className="p-4">
+                  <RatingCard
+                    title="Score"
+                    value={score}
+                    icon={faStar}
+                    color="text-yellow-500"
+                  />
+                </div>
               </div>
             </div>
           </div>
--- movie-review-platform-frontend/src/pages/MovieInfoPage/RatingCard.tsx
+++ movie-review-platform-frontend/src/pages/MovieInfoPage/RatingCard.tsx
@@ -1,9 +1,25 @@
 import { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
-const RatingCard: FC = () => {
+interface RatingCardProps {
+  title: string;
+  value: number;
+  icon: IconDefinition;
+  color: string;
+}
+
+const RatingCard: FC<RatingCardProps> = ({ title, value, icon, color }) => {
   return (
-    <div>
-      <h1>Hi there</h1>
+    <div className="bg-white p-4 rounded-lg border-2 border-slate-300 shadow-md transition-transform duration-300 hover:shadow-button-primary-bg">
+      <div className="text-2xl font-bold mb-2 text-black">{title}</div>
+      <hr className="border-t border-gray-300 mb-4" />
+
+      <div className="flex text-center justify-center items-center">
+        <span className={`text-4xl font-bold ${color}`}>
+          {value} <FontAwesomeIcon icon={icon} />
+        </span>
+      </div>
     </div>
   );
 };
